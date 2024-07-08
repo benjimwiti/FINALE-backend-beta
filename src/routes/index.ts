41918cb2230642494b3f7f99f1731e8916ab7b08
benjimwiti <167991485+benjimwiti@users.noreply.github.com>
2024-07-08 19:07:02 +0300

    auto deploy handled by github workflow

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,6 @@ import {Express, Request, Response} from "express"
 
 export function registerRoutes(app: Express) {
     app.get("/health", (req: Request, res: Response) => {
-        res.status(200).json({ message: "Server is running properly" });
+        res.status(200).json({ message: "Server is running properly updated" });
     });
 }
